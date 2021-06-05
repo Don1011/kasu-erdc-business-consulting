66c731e8779ce6de707d498bd6ade26ecebfeb83
Don1011 <dondaniel028@gmail.com>
2021-06-05 20:44:21 +0100

    Resolving missing IP_ADDRESS variable error in index.js

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ if(process.env.NODE_ENV === 'production'){
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-    console.log(`Server running on ${IP_ADDRESS}:${port}`);
+    console.log(`Server running on port ${port}`);
 })
